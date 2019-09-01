b7d0abefffd8e2b22feccb7592a98e2ddcd66024
Matheus Aguilar <matheusdeoliveira.ti@gmail.com>
2019-09-01 16:21:53 -0300

    Leitura das Teclas

--- php/implicito.php
+++ php/implicito.php
@@ -17,11 +17,61 @@
 
 <head>
     <script type="text/javascript">
+        
+        var indice = -1;
         var palavras = JSON.parse('<?=$palavrasJSON?>')
         console.log(palavras);
+
+        //Parte que muda as palavras da DIV
+        function mudaPalavra(){
+            if(indice >= palavras.length){
+                document.getElementById("palavra").style.color = 'black';
+                document.getElementById("palavra").innerHTML = "fim";
+            }
+            else{
+                document.getElementById("palavra").style.color = 'black';
+                document.getElementById("palavra").innerHTML = indice + " " + palavras[indice].nome;
+            }
+        }
+
+        //Parte de captura das teclas
+        document.onkeyup = function (event) {
+            
+            var key = event.which || event.keyCode;
+
+            if(indice >= palavras.length){
+                return;
+            }
+            else if(indice == -1){
+                if(key == 13){
+                    indice++;
+                    mudaPalavra();
+                }
+            }
+            //Seta esquerda
+            else if(key == 37){
+                if(palavras[indice].categoria == 1){
+                    indice++;
+                    mudaPalavra();
+                }
+                else{
+                    document.getElementById("palavra").style.color = 'red';
+                }
+            }
+            //Seta direita
+            else if(key == 39){
+                if(palavras[indice].categoria == 2){
+                    indice++;
+                    mudaPalavra();
+                }
+                else{
+                    document.getElementById("palavra").style.color = 'red';
+                }
+            }
+        }
     </script>
 </head>
 
 <body>
-    <!--Insira seu front-end aqui-->
+    <div id = "palavra" onkeydown="apertouTecla(event)">Aperte enter para começar...</div>
 </body>
